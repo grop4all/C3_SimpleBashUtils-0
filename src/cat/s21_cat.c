e987046f0eded949a178c92c623518fade9595e2
henschel <henschel@student.21-school.ru>
2022-06-22 01:36:27 +0500

    grep

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -2,7 +2,7 @@
 
 
 int main(int argc, char **argv) {
-    s21_cat (argc, argv);
+    s21_cat(argc, argv);
     return 0;
 }
 
@@ -10,7 +10,7 @@ void s21_cat(int argc, char **argv) {
     list_flag flag;
     int len_file_list = 0;
     char ** file_list = malloc(sizeof(char *) * (argc));
-    if (parser(argc, argv, &flag, file_list, &len_file_list)){
+    if (parser(argc, argv, &flag, file_list, &len_file_list)) {
         for (int i = 0; i < len_file_list; ++i) {
             FILE *file = fopen(file_list[i], "r");
             if (errno) {
@@ -21,22 +21,19 @@ void s21_cat(int argc, char **argv) {
                 write_file(file, &flag, 0);
                 write_file(file, &flag, 1);
                 fclose(file);
-            }    
+            }
         }
     }
     free(file_list);
 }
 
 int parser(int argc, char ** argv, list_flag *flag, char** file_list, int *len_file_list) {
-
     init(flag);
     int err = 0;
-
     if (check_file(argc, argv, file_list, len_file_list) && check_flag(argc, argv, flag))
         err = 1;
-    
 #ifdef DEBAG
-    wr_flag_debag(flag);    
+    wr_flag_debag(flag);
 #endif
     return err;
 }
@@ -50,21 +47,18 @@ void init(list_flag *flag) {
 }
 
 void wr_flag_debag(list_flag *flag) {
-
-    printf("MARK_END_LINES %i\n",flag->MARK_END_LINES);
-    printf("REMOVE_BLANK_LINES %i\n",flag->REMOVE_BLANK_LINES);
-    printf("NUMBER_NOBLANK_LINES %i\n",flag->NUMBER_NOBLANK_LINES);
-    printf("NUMBER_LINE %i\n",flag->NUMBER_LINE);
-    printf("REPLACE_TAB %i\n",flag->REPLACE_TAB);
+    printf("MARK_END_LINES %i\n", flag->MARK_END_LINES);
+    printf("REMOVE_BLANK_LINES %i\n", flag->REMOVE_BLANK_LINES);
+    printf("NUMBER_NOBLANK_LINES %i\n", flag->NUMBER_NOBLANK_LINES);
+    printf("NUMBER_LINE %i\n", flag->NUMBER_LINE);
+    printf("REPLACE_TAB %i\n", flag->REPLACE_TAB);
 }
 
 int check_flag(int argc, char ** argv, list_flag *flag) {
-
-    char msg [] = "Try 'cat --help' for more information.";
     char c;
     int ind = 0;
     int err = 0;
-    while((c = getopt_long(argc, argv, "enbstv", long_options, &ind)) != -1){
+    while ((c = getopt_long(argc, argv, "enbstv", long_options, &ind)) != -1) {
         err = 1;
         if (c == 'n')
             flag->NUMBER_LINE = 1;
@@ -76,11 +70,8 @@ int check_flag(int argc, char ** argv, list_flag *flag) {
             flag->REPLACE_TAB = 1;
         else if (c == 'e')
             flag->MARK_END_LINES = 1;
-        else if (c == '?'){
-            fprintf(stderr, "%s\n", msg);
+        else if (c == '?')
             err = 0;
-            break;
-        }
         else
             err = 0;
     }
@@ -89,24 +80,21 @@ int check_flag(int argc, char ** argv, list_flag *flag) {
 }
 
 int check_file(int argc, char ** argv, char ** file_list, int *len_file_list) {
-
     int err = 0;
     for (int i = 1; i < argc; ++i)
-        if (strspn(argv[i], "-") != 1 && strspn(argv[i], "--") != 2){
+        if (strspn(argv[i], "-") != 1 && strspn(argv[i], "--") != 2) {
             file_list[*len_file_list] = argv[i];
             *len_file_list +=1;
         }
 #ifdef DEBAG
-    for (int i = 0; i < *len_file_list ;++i)
-        printf("%s \n", file_list[i] );
+    for (int i = 0; i < *len_file_list; ++i)
+        printf("%s \n", file_list[i]);
 #endif
-
-    return (err == *len_file_list) ? -1 : 1; 
+    return (err == *len_file_list) ? -1 : 1;
 }
 
 
-void write_file(FILE * file, list_flag *flag, int reset){
-
+void write_file(FILE * file, list_flag *flag, int reset) {
     static cat_struct c = {
         .buffer = {},
         .line = 1,
@@ -114,13 +102,12 @@ void write_file(FILE * file, list_flag *flag, int reset){
         .prev_prev = 0,
         .prev = 0,
     };
-
     if (reset) {
 #if defined(__MACH__) || defined(__APPLE__)
     c.line = 1;
 #endif
     }
-    while (feof(file) != EOF && file){
+    while (feof(file) != EOF && file) {
         int ch = getc(file);
         if (ch == EOF)
             break;
@@ -133,13 +120,13 @@ void write_file(FILE * file, list_flag *flag, int reset){
         mark_end_lines(ch, &c, flag);
         replace_tabs(ch, &c, flag);
         fprintf(stdout, "%s", c.buffer);
-        memset(c.buffer,'\0', MAX_CHAR_LEN);
-    }   
+        memset(c.buffer, '\0', MAX_CHAR_LEN);
+    }
     if (errno)
         perror("Error: ");
 }
 
-int remove_blank_lines (char ch, cat_struct *c, list_flag *flag){
+int remove_blank_lines(char ch, cat_struct *c, list_flag *flag) {
     int ans = 0;
     if (ch == '\n' && c->prev_prev == '\n' && c->prev == '\n' &&
         flag->REMOVE_BLANK_LINES)
@@ -148,7 +135,7 @@ int remove_blank_lines (char ch, cat_struct *c, list_flag *flag){
 }
 
 void mark_end_lines(char ch, cat_struct *c, list_flag *flag) {
-    if (flag->MARK_END_LINES && ch == '\0' ) {
+    if (flag->MARK_END_LINES && ch == '\0') {
         c->buffer[0] = '$';
         c->buffer[1] = '\n';
     }
@@ -158,7 +145,7 @@ void mark_end_lines(char ch, cat_struct *c, list_flag *flag) {
     }
 }
 
-void replace_tabs(char ch, cat_struct *c, list_flag *flag){
+void replace_tabs(char ch, cat_struct *c, list_flag *flag) {
     if (flag->REPLACE_TAB && ch == '\t') {
         c->buffer[0] = '^';
         c->buffer[1] = 'I';
@@ -166,14 +153,14 @@ void replace_tabs(char ch, cat_struct *c, list_flag *flag){
 }
 
 void number_lines(char ch, cat_struct *c, list_flag *flag) {
-    if (flag->NUMBER_LINE && c->nl){
+    if (flag->NUMBER_LINE && c->nl) {
         fprintf(stdout, "%6d\t", c->line);
         c->line++;
         c->nl = 0;
     }
     if (ch == '\n')
         c->nl = 1;
-    if (flag->NUMBER_NOBLANK_LINES && c->nl && ch != '\n'){
+    if (flag->NUMBER_NOBLANK_LINES && c->nl && ch != '\n') {
         fprintf(stdout, "%6d\t", c->line);
         c->line++;
         c->nl = 0;
--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -1,3 +1,6 @@
+#ifndef SRC_CAT_S21_CAT_H_
+#define SRC_CAT_S21_CAT_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
@@ -7,20 +10,19 @@
 #define _GNU_SOURCE
 #define MAX_CHAR_LEN 6
 
-struct list_flag
-{
-    int NUMBER_LINE; // нумеруем все строки
-    int REMOVE_BLANK_LINES; // удалаяем пустые строки
-    int NUMBER_NOBLANK_LINES; // нумеруем только не пустыне строки
-    int MARK_END_LINES; // отмечаем конец строки 
-    int REPLACE_TAB; // заменям tab
+struct list_flag {
+    int NUMBER_LINE;
+    int REMOVE_BLANK_LINES;
+    int NUMBER_NOBLANK_LINES;
+    int MARK_END_LINES;
+    int REPLACE_TAB;
 };
 
 struct option long_options[] = {
     {"number", 0, 0, 'n'},
-    {"number-nonblank", 0, 0, 'b'},        
+    {"number-nonblank", 0, 0, 'b'},
     {"squeeze-blank", 0, 0, 's'},
-    };
+};
 
 
 struct cat_struct {
@@ -34,15 +36,17 @@ struct cat_struct {
 typedef struct list_flag list_flag;
 typedef struct cat_struct cat_struct;
 
-int parser (int, char **, list_flag *, char **, int *);
-void s21_cat (int, char **);
+int parser(int, char **, list_flag *, char **, int *);
+void s21_cat(int, char **);
 int check_flag(int, char **, list_flag *);
 int check_long_flag(int, char **, list_flag *);
 void wr_flag_debag(list_flag *);
 void init(list_flag *);
-int remove_blank_lines (char ch, cat_struct *c, list_flag *flag);
+int remove_blank_lines(char ch, cat_struct *c, list_flag *flag);
 void mark_end_lines(char ch, cat_struct *c, list_flag *flag);
 void replace_tabs(char ch, cat_struct *c, list_flag *flag);
 void number_lines(char ch, cat_struct *c, list_flag *flag);
 void write_file(FILE * file, list_flag *flag, int reset);
 int check_file(int argc, char ** argv, char ** file_list, int *len_file_list);
+
+#endif  //  SRC_CAT_S21_CAT_H_
